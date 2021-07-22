e03fd6a58b53a1d324d62565caed34698642ce44
KhuongNguyen611 <50842187+KhuongNguyen611@users.noreply.github.com>
2021-07-22 13:50:42 +0700

    Tiny fix
    -Missing function resetPassword

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token');
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 export default router;
