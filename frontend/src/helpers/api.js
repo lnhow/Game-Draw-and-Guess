2f52128c927d5eb73560ba7a89598501678fea17
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-07-26 10:57:00 +0700

    Update API to use .env variables

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_API
   ? process.env.REACT_APP_API
-  : 'http://localhost:5000';
+  : 'http://localhost:5000/api';
 
 const api = axios.create({ baseURL });
 
