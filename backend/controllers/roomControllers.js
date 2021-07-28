eee2f8b2a35bd7bda3eef9ed78cffe7ec3e298dd
Hieu-mai-2000 <hieu.mainguyenminh@hcmut.edu.vn>
2021-07-28 15:03:35 +0700

    Get Category For Room

--- backend/controllers/roomControllers.js
+++ backend/controllers/roomControllers.js
@@ -1,6 +1,7 @@
 import gameroomModel from '../models/gameroomModel.cjs';
+import categoryModel from '../models/categoryModel.cjs';
 
-const roomController = { findingRoom };
+const roomController = { findingRoom, getCategoryForRoom };
 
 async function findingRoom(req, res) {
   try {
@@ -8,6 +9,26 @@ async function findingRoom(req, res) {
     res.json(all);
   } catch (err) {
     res.status(500).json({ msg: "Can't find any room!" });
+  }
+}
+
+async function getCategoryForRoom(req, res) {
+  try {
+    const categories = await categoryModel.find();
+    if (categories.length === 0)
+      res.status(404).json({ message: "Can't find category" });
+
+    const categoriesRefactor = categories.map((category) => ({
+      id: category._id,
+      name: category.categoryName,
+    }));
+
+    res.status(200).json({
+      message: 'successfully',
+      categories: categoriesRefactor,
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'error from server' });
   }
 }
 
