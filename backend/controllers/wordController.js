e0a8ae8f0078deb865454e850b3d6feee5b7c936
fairypham <namnd.tth@gmail.com>
2021-07-29 10:58:52 +0700

    update API CRUD room

---
+++ backend/controllers/wordController.js
@@ -0,0 +1,80 @@
+import wordModel from '../models/wordModel.cjs';
+
+const wordController = {
+  getWords,
+  createWord,
+  deleteWord,
+  updateWord,
+};
+
+async function getWords(req, res) {
+  try {
+    const all = await wordModel.find();
+    res.json(all);
+  } catch (err) {
+    res.status(500).json({ message: "Can't find any word!" });
+  }
+}
+
+async function createWord(req, res) {
+  const existedWword = await wordModel.findOne({
+    id: req.body._id,
+  });
+
+  if (existedWword) {
+    res.status(400).json({ message: 'Word already exists!' });
+  }
+
+  const word = new wordModel(req.body);
+
+  try {
+    await word.save();
+
+    res.status(200).json({
+      message: 'Create word successfully',
+      word: word.word,
+      category: word.category,
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+}
+
+async function updateWord(req, res) {
+  try {
+    const word = wordModel.findByIdAndUpdate(req.params.id);
+
+    if (!word) {
+      res.status(404).json({ message: 'Word not found!' });
+    } else {
+      word.category = req.body.category;
+      word.word = req.body.word;
+      word.save();
+      res.status(200).json({
+        wordId: word._id,
+        category: word.category,
+        message: 'Update word successfully!',
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Invalid word. Cannot update word!' });
+  }
+}
+
+async function deleteWord(req, res) {
+  try {
+    const word = wordModel.findByIdAndDelete(req.params.id);
+
+    if (!word) {
+      res.status(404).json({ message: 'Word does not exist!' });
+    }
+
+    await word.save();
+
+    res.status(200).json({ message: 'Delete word successfully!' });
+  } catch (err) {
+    res.status(500).json({ message: 'Invalid word. Cannot delete word!' });
+  }
+}
+
+export default wordController;
