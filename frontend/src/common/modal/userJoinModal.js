ed79fa0d29d282dacb51947bf4673570e65c0913
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-08-09 09:56:49 +0700

    Create userJoinModal.js

---
+++ frontend/src/common/modal/userJoinModal.js
@@ -0,0 +1,76 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+const center = {
+  display: 'table',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+};
+
+export default function GuessJoinRoomModal({
+  isOpen,
+  closeAlert,
+  join,
+  errorMessage,
+}) {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [value, setValue] = React.useState('');
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = '#444444';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <Modal
+        isOpen={isOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeAlert}
+        style={customStyles}
+      >
+        <h1 style={{ color: 'red' }}>{errorMessage}</h1>
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Join as guest</h2>
+        <form>
+          <label>Username</label>&emsp;
+          <input
+            value={value}
+            onChange={(e) => setValue(e.currentTarget.value)}
+          />
+        </form>
+        <div style={center}>
+          <Button variant="contained" color="warning" onClick={closeAlert}>
+            cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => join(value)}
+          >
+            join
+          </Button>
+        </div>
+      </Modal>
+    </div>
+  );
+}
