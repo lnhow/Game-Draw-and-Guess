2c2538d14431e7e3ac0886e49bc4d00abb7d9579
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-08-11 09:23:00 +0700

    Fix draw data being send back to the drawer

--- backend/socket/handlers/drawData.handler.js
+++ backend/socket/handlers/drawData.handler.js
@@ -3,7 +3,7 @@ import RoomSocket from '../controllers/room.js';
 const handleCanvasData = (io, socket, drawData, callback) => {
   const user = RoomSocket.getUserBySocketId(socket.id);
   if (user) {
-    io.to(user.roomId).emit('canvas-data', drawData);
+    socket.to(user.roomId).emit('canvas-data', drawData);
   }
   if (callback) {
     callback();
